04d1811356d14407f02fec28b89c8fdf63ed6b0a
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-09-25 09:57:07 -0700

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+
+        unordered_map<char, int> fMap;
+
+        for (const char c : s1) {
+            fMap[c]++;
+        }
+        int count = s1.size();
+
+        int lo = 0, hi = 0;
+
+        while (hi < s2.size()) {
+            if (count == 0) return true;
+
+            if (fMap.find(s2[hi]) == fMap.end()) {
+                if (hi == lo) {
+                    hi++;
+                    lo++;
+                    continue;
+                }
+                while (lo != hi) {
+                    fMap[s2[lo]]++;
+                    count++;
+                    lo++;
+                }
+            }else {
+                while (fMap[s2[hi]] == 0) {
+                    fMap[s2[lo]]++;
+                    count++;
+                    lo++;
+                }
+
+                fMap[s2[hi]]--;
+                hi++;
+                count--;
+            }
+        }
+
+        if (count == 0) return true;
+        return false;
+    }
+};
